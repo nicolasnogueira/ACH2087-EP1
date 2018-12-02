ad4ab9d6bf8a2b5f2028c058c998fec54d6ebda3
Victor <taendy.s@usp.br>
2018-12-02 15:58:42 -0200

    shellscript builder project

---
+++ teste.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#chmod 777 teste.sh
+
+
+if [ "$1" = "antlr4" ]; then
+	java -jar /usr/local/lib/antlr-4.7.1-complete.jar -o ./build/ Cmenos.g4
+
+elif [ "$1" = "compile" ]; then
+	javac -sourcepath $(pwd)/build/ -cp /usr/local/lib/antlr-4.7.1-complete.jar -d $(pwd)/build/cbuild/ $(pwd)/build/*.java
+
+elif [ "$1" = "exec" ]; then
+	#echo $(pwd)/build/cbuild/
+	java -cp $(pwd)/build/cbuild/:/usr/local/lib/antlr-4.7.1-complete.jar:. org.antlr.v4.gui.TestRig Cmenos prog -gui < input
+
+elif [ "$1" = "path" ]; then
+
+	if [ ! -d $(pwd)"/build" ]; then
+	mkdir build
+	mkdir build/cbuild
+
+	else
+		if [ ! -d $(pwd)"/build/cbuild" ]; then
+			mkdir build/cbuild
+		fi
+	fi
+
+elif [ "$1" = "help" ]; then
+	echo '$ ./teste.sh path \t //check directory struct '
+	echo '$ ./teste.sh antlr4 \t //generate project glc \(Cmenos.g4\)'
+	echo '$ ./teste.sh compile \t //compile project glc \(Cmenos.g4\)'
+	echo '$ ./teste.sh exec \t //run project glc \(Cmenos.g4\)'
+fi
+
+
+
+
